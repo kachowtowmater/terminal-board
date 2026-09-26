d97a24e8f1dc373cd4120bcd21f63e59738e02b0
Charles Chen <kachowtowmater@gmail.com>
2026-09-25 19:41:00 -0700

    fix(#178): import store::actors::Identity for force_check

--- src/store.rs
+++ src/store.rs
@@ -14,6 +14,7 @@
 
 use crate::hooks;
 use crate::lock;
+use crate::store::actors::Identity;
 use rusqlite::types::Type;
 use rusqlite::{params, Connection, OptionalExtension, TransactionBehavior};
 use serde::Serialize;
